c8aaf7cef2d27d7debc0fe7690c66b13ac3d1591
Gabriel Horner <gabriel.horner@gmail.com>
2009-02-20 06:12:28 -0500

    plugin flexible enough to take an existing tag class

--- init.rb
+++ init.rb
@@ -1,5 +1,13 @@
 require 'has_machine_tags'
-require 'has_machine_tags/tag'
-require 'has_machine_tags/tagging'
+require 'has_machine_tags/tag_methods'
 
+#attempt to load constant
+::Tag rescue nil
+if Object.const_defined? :Tag
+  ::Tag.class_eval %[include HasMachineTags::TagMethods]
+else
+  require 'has_machine_tags/tag'
+end
+
+require 'has_machine_tags/tagging'
 ActiveRecord::Base.send :include, HasMachineTags

20e2b155f557868d276865a463baf2208b1cbc5a
Gabriel Horner <gabriel.horner@gmail.com>
2010-06-10 19:15:12 -0400

    got rid of $: manipulation

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,7 +3,6 @@ require 'activerecord'
 require 'test/unit'
 require 'context' #gem install jeremymcanally-context -s http://gems.github.com
 require 'matchy' #gem install jeremymcanally-matchy -s http://gems.github.com
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require File.join(File.dirname(__FILE__), '..', 'init')
 
 #Setup logger

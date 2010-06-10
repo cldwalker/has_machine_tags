8ae5cae06cc208dbb243be22510a4b3778de5571
Gabriel Horner <gabriel.horner@gmail.com>
2010-06-10 19:19:16 -0400

    deleted rails/

--- init.rb
+++ init.rb
@@ -1,1 +1,13 @@
-require File.join(File.dirname(__FILE__), 'rails', 'init.rb')
+require 'has_machine_tags'
+require 'has_machine_tags/tag_methods'
+
+#attempt to load constant
+::Tag rescue nil
+if Object.const_defined? :Tag
+  ::Tag.class_eval %[include HasMachineTags::TagMethods]
+else
+  require 'has_machine_tags/tag'
+end
+
+require 'has_machine_tags/tagging'
+ActiveRecord::Base.send :include, HasMachineTags
